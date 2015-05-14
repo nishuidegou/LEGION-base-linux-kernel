acbbdbfa4bf17d99736d7e8da0c7244acadd4049
nishuidegou <emb_zb@sina.cn>
2015-05-14 09:29:49 +0800

    Update legion.h

--- legion.h
+++ legion.h
@@ -4,6 +4,9 @@
 #ifndef __LEGION_H
 #define __LEGION_H
 
+/*
+ *
+ */
 typedef struct{
  double x;   // value of oscillator
  double rou; // gaussian noise term
@@ -11,12 +14,19 @@ typedef struct{
  double s;   // coupling from other oscillators
 }Oscillator_Struct;
 
+/*
+ *
+ */
 typedef struct{
  double ata;
  double gama;
  double beta;
 }Inhibitory_Struct;
 
+/*
+ * connection topology,the topology decide the network dimension
+ * connection weight
+ */
 
 
 
